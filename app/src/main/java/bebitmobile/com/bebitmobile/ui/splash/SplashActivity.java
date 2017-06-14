17bbbf2656b8a701c1a7627785ec1fc480f30e60
dev3pradiptamobile <dev3.pradipta.mobile@gmail.com>
2017-06-14 07:47:14 +0700

    Contoh Butterknife in Splashscreen

--- app/src/main/java/bebitmobile/com/bebitmobile/ui/splash/SplashActivity.java
+++ app/src/main/java/bebitmobile/com/bebitmobile/ui/splash/SplashActivity.java
@@ -3,10 +3,14 @@ package bebitmobile.com.bebitmobile.ui.splash;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
 
 import bebitmobile.com.bebitmobile.R;
 import bebitmobile.com.bebitmobile.ui.BaseActivity;
 import bebitmobile.com.bebitmobile.ui.signinup.LoginActivity;
+import butterknife.Bind;
 
 /**
  * Modules of bebitmobile.com.bebitmobile.ui.activity
@@ -19,6 +23,10 @@ import bebitmobile.com.bebitmobile.ui.signinup.LoginActivity;
 
 public class SplashActivity extends BaseActivity {
 
+    /*CONTOH BUTTERKNIFE*/
+//    @Bind(R.id.img_bebit)
+//    ImageView imgBebit;
+
     @Override
     protected int getLayout() {
         return R.layout.a_splash;
@@ -27,6 +35,8 @@ public class SplashActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /*CONTOH BUTTERKNIFE*/
+//        imgBebit.setVisibility(View.INVISIBLE);
 
         new Handler().postDelayed(new Runnable() {
             @Override
